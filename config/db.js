c336d483873cab2ed1dc390bc1be2b94307aaa88
Rajan <rajan.rana6@gmail.com>
2024-10-04 19:58:44 +0530

    backend and calling api

--- config/db.js
+++ config/db.js
@@ -1,16 +1,14 @@
 import mongoose from "mongoose";
-import colors from "colors"
-
+import colors from "colors";
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URL)
-        console.log(`connected to mongodb ${conn.connection.host}`.bgMagenta.white);
-
+        const conn = await mongoose.connect(process.env.MONGO_URL);
+        console.log(
+            `Conneted To Mongodb Databse ${conn.connection.host}`.bgMagenta.white
+        );
     } catch (error) {
-        console.log(`error in mongo db ${error}`.bgRed.white);
-
+        console.log(`Errro in Mongodb ${error}`.bgRed.white);
     }
-}
+};
 
 export default connectDB;
-
--- server.js
+++ server.js
@@ -1,37 +1,38 @@
-import express from 'express';
-import colors from 'colors';
-import dotenv from 'dotenv';
-import morgan from 'morgan';
-import connectDB from './config/db.js';
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
 
+//configure env
+dotenv.config();
 
-//config env
-dotenv.config()
+//databse config
+connectDB();
 
-//database config
-
-connectDB()
-
-//rest object 
+//rest object
 const app = express();
 
-//middleware
-app.use(express.json())
-app.use(morgan('dev'))
+//middelwares
+app.use(express.json());
+app.use(morgan("dev"));
+
+//routes
+app.use("/api/v1/auth", authRoutes);
 
 //rest api
-app.get('/', (req, res) => {
-    res.send({
-        massage: "<h1>welcome to e-commerce website</h1>"
-    })
-})
+app.get("/", (req, res) => {
+    res.send("<h1>Welcome to ecommerce app</h1>");
+});
 
-//port
+//PORT
+const PORT = process.env.PORT || 8080;
 
-const PORT = process.env.PORT;
-
-//run the appplication
-
+//run listen
 app.listen(PORT, () => {
-    console.log(`server running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white);
-})
+    console.log(
+        `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
+            .white
+    );
+});
